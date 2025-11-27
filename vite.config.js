e9abd07274956b1dbacb935a0c5ce6b49ad160aa
Jacopo Peroni <jacopoperoni@wl-pool2-pnt-25718.uni-muenster.de>
2025-11-27 07:49:51 +0100

    minor

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-   import { defineConfig } from 'vite'
-   import react from '@vitejs/plugin-react'
-   import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-   export default defineConfig({
-     base: '/site/',
-     plugins: [react()],
-     resolve: {
-       alias: {
-         '@': path.resolve(__dirname, './src'),
-       },
-     },
-   })
+export default defineConfig({
+  base: '/site/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    middlewareMode: true,
+  },
+})
